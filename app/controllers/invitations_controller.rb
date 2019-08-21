ee30ec523d30b79ad7fbcb4bf5333a96771cbb96
Theinthinzar <thinzarthein@mm.cybermissions.co.jp>
2019-08-21 13:35:53 +0630

    finish

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -11,14 +11,12 @@ class InvitationsController < ApplicationController
     @user1.user_email = params[:invitations][:user_email1]
     if @user1.user_email != ""
       UserMailer.signup_confirmation(@user1, session[:workspace_id], session[:workspace_name]).deliver
-      redirect_to home_path
     end
 
     @user2 = MUser.new()
     @user2.user_email = params[:invitations][:user_email2]
     if @user2.user_email != ""
       UserMailer.signup_confirmation(@user2, session[:workspace_id], session[:workspace_name]).deliver
-      redirect_to home_path
     end
 
     @user3 = MUser.new()
@@ -26,8 +24,9 @@ class InvitationsController < ApplicationController
     @workspace3 = MWorkspace.new()
     if @user3.user_email != ""
       UserMailer.signup_confirmation(@user3, session[:workspace_id], session[:workspace_name]).deliver
-      redirect_to home_path
     end
+
+    redirect_to home_path
   end
 
   def edit
