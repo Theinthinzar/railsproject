3387892c7c80b75a6d3dcb62f34a065146da85b3
Theinthinzar <thinzarthein@mm.cybermissions.co.jp>
2019-08-26 11:18:46 +0630

    Finish

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,8 @@ module SlackApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-<<<<<<< HEAD
+
     config.time_zone = "Rangoon"
     config.active_record.default_timezone = :local
-=======
-    
->>>>>>> ae6d9934d9f0c5205c5093f474a9778c79702a79
   end
 end
